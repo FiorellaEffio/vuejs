a2fcea5c017ad05215247ce908612de5bc27f5f9
Aweba13 <fiorella.effio@unmsm.edu.pe>
2018-10-10 17:53:40 -0500

    2nd part of data binding

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ new Vue({
   el:'#vue-app',
   data: {
     name: 'Fiorella',
-    website: 'https://thenetninja.co.uk'
+    website: 'https://thenetninja.co.uk',
+    websiteTag: '<a href="https://thenetninja.co.uk">The net ninja TAG</a>'
   },
   methods: {
     greet: function(time) {
